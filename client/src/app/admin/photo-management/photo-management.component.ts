22f368d1fc2ebd77b7066374190d4ce62cc3bd1b
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-08-17 23:29:17 -0700

    fixed photo management

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -8,29 +8,31 @@ import { AdminService } from 'src/app/_services/admin.service';
   styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
-
   photos: Photo[];
+
   constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.getPhotosForApproval();
   }
 
-  getPhotosForApproval(){
+  getPhotosForApproval() {
     this.adminService.getPhotosForApproval().subscribe(photos => {
       this.photos = photos;
     })
   }
 
-  approvePhoto(photoId){
+  approvePhoto(photoId) {
     this.adminService.approvePhoto(photoId).subscribe(() => {
       this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
     })
   }
 
-  rejectPhoto(photoId){
+  rejectPhoto(photoId) {
     this.adminService.rejectPhoto(photoId).subscribe(() => {
       this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
     })
   }
+
 
 }
